19b97215d1e97ad73fb0f845b371af54affb5930
felixgarcia97 <92739396+felixgarcia97@users.noreply.github.com>
2021-10-22 22:13:24 -0500

    Update login.js

--- login.js
+++ login.js
@@ -2,13 +2,11 @@ function validar(){
    var us,co;
    us=document.getElementById("usuario").value;
    co=document.gtElementById("contraseña").value;
-   
    if(us=="Felix"&&co=="mendoza"){
-   	
-   	alert("Tu usuario y contraseña son validos");
-   	window.open("peso.html");
-}
-else{
-	alert("Tu usuario y contraseña no son validos");
-}
+       alert("Tu usuario y contraseña son validos");
+       window.open("peso.html");
+   }
+   else{
+       alert("Tu usuario y contraseña no son validos");
+   }
 }
